2c1e547cd4970d1801e8465f0206da6e4345f51c
SANVITO_LORENZO <lorisanvi2000@gmail.com>
2024-05-30 11:45:49 +0200

    aggiunta progetto

---
+++ StudentPortal/StudentPortal.web/Controllers/StudentsController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentPortal.web.Data;
+using StudentPortal.web.Models;
+using StudentPortal.web.Models.Entities;
+
+namespace StudentPortal.web.Controllers
+{
+    public class StudentsController : Controller
+
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public StudentsController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddStudentsViewModel viewModel)
+        {
+            var student = new Student
+            {
+                Name = viewModel.Name,
+                Email = viewModel.Email,
+                Phone = viewModel.Phone,
+                Subscribed = viewModel.Subscribed
+            };
+            await dbContext.students.AddAsync(student);
+            await dbContext.SaveChangesAsync();
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            var students = await dbContext.students.ToListAsync();
+            return View(students);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var students = await dbContext.students.FindAsync(id);
+            return View(students);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Student viewModel)
+        {
+            var student = await dbContext.students.FindAsync(viewModel.Id);
+
+            if (student is not null)
+            {
+                student.Name = viewModel.Name;
+                student.Email = viewModel.Email;
+                student.Phone = viewModel.Phone;
+                student.Subscribed = viewModel.Subscribed;
+
+                await dbContext.SaveChangesAsync();
+            }
+
+            return RedirectToAction("List", "Students");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Student viewModel)
+        {
+            var student = await dbContext.students.FindAsync(viewModel.Id);
+            if (student is not null)
+            {
+                dbContext.students.Remove(student);
+                await dbContext.SaveChangesAsync();
+            }
+
+            return RedirectToAction("List", "Students");
+        }
+    }
+}
---
+++ StudentPortal/StudentPortal.web/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentPortal.web.Models.Entities;
+
+namespace StudentPortal.web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //COSTRUTTORE
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        //ATTRIBUTI
+        public DbSet<Student> students { get; set; }
+    }
+}
