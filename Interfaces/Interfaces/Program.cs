8241b3e2f27ec6dbba6829ca49dfa71518c4effc
SANVITO_LORENZO <lorisanvi2000@gmail.com>
2024-05-31 09:18:47 +0200

    .

---
+++ Interfaces/Interfaces/Program.cs
@@ -0,0 +1,14 @@
+﻿using interfaces;
+
+
+veleno veleno = new veleno();
+HandlePick(veleno);
+
+spada spada = new spada();
+HandlePick(spada);
+
+
+void HandlePick(IPickup pickup)
+{
+    pickup.Pickup();
+}
---
+++ Interfaces/Interfaces/objects.cs
@@ -0,0 +1,42 @@
+﻿using interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interfaces
+{
+    internal class spada : IPickup
+    {
+        public void Pickup()
+        {
+            Console.WriteLine("SPADA!");
+        }
+    }
+
+    internal class FUCILE : IPickup
+    {
+        public void Pickup()
+        {
+            Console.WriteLine("FUCILE!");
+        }
+    }
+
+
+    internal class veleno : IPickup
+    {
+        public void Pickup()
+        {
+             Console.WriteLine("VELENO!");
+        }
+    }
+
+    internal class orbomagico : IPickup
+    {
+        public void Pickup()
+        {
+            Console.WriteLine("ORBO!");
+        }
+    }
+}
