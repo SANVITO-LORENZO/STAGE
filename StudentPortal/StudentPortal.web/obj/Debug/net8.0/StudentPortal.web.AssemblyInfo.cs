bbb20ebc3f602af84646c03decd5fb0a728a44be
SANVITO_LORENZO <lorisanvi2000@gmail.com>
2024-06-04 17:09:21 +0200

    .

--- StudentPortal/StudentPortal.web/obj/Debug/net8.0/StudentPortal.web.AssemblyInfo.cs
+++ StudentPortal/StudentPortal.web/obj/Debug/net8.0/StudentPortal.web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentPortal.web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5532be8bc981095b6306eda308dd8ee3940e9e6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+687e449291df85f129f738f40ccc62ea15450d6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentPortal.web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentPortal.web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
